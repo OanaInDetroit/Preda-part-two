d1dd7d646e665a98cc64438ef56a8fbca8eee933
OanaInDetroit <66800153+OanaInDetroit@users.noreply.github.com>
2020-09-11 10:16:16 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var i = prompt('How many inches of rain fell?'); 
+  console.log(i); 
+
+  for (let i=0; i< 30; i++){
+      document.write(i + "</br/1>"); 
+  }
+
+var UseFert = prompt('Did you use fertilizer?'); 
+  console.log((UseFert); 
+
+ 
